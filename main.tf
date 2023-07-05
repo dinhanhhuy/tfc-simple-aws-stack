58f404962bc7d4dcec18fbaafa430c5cbad562b8
huyda <huyda@example.com>
2023-07-05 14:23:04 +0700

    feat: simple ec2 block

--- main.tf
+++ main.tf
@@ -2,17 +2,8 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-data "aws_ami" "linux" {
-  owners      = ["self"]  # Change to the list of AWS account IDs if the AMI is owned by different accounts
-  most_recent = true
-  filter {
-    name   = "image-id"
-    values = ["ami-0c91f4476780c2eaf"]
-  }
-}
-
 resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.linux.id}"
+  ami           = "ami-0c91f4476780c2eaf"
   instance_type = "t2.micro"
 
   tags = {
